d0dabe37456a1e51ea940fc6fabc002b59e17811
Rafal Zarajczyk <rzarajczyk@gmail.com>
2022-04-26 23:33:04 +0200

    gh actions && service-bootstrap

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from homie_helpers import MqttSettings
 
 from SonyBravia import SonyBravia
-from bootstrap import start_service
+from bootstrap.bootstrap import start_service
 
-config, logger = start_service()
+config, logger, timezone = start_service()
 
-scheduler = BlockingScheduler(timezone=config['timezone'])
+scheduler = BlockingScheduler(timezone=timezone)
 
 device = SonyBravia(config=config['sony-bravia'], mqtt_settings=MqttSettings.from_dict(config['mqtt']))
 
